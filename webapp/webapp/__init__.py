55514cbf5e817ad56b9c617d3f94d6f8f3917ff6
L-seaung <2393943608@qq.com>
2017-07-27 17:14:34 +0800

    remove requirement.txt and create new.html, register.html, _backup.py

--- webapp/webapp/__init__.py
+++ webapp/webapp/__init__.py
@@ -11,61 +11,9 @@ def create_app(object_name):
     app.config.from_object(object_name)
     db.init_app(app)
     bcrypt.init_app(app)
-    
+
     app.register_blueprint(blog_blueprint)
     return app
-
-@app.route('/')
-@app.route('/<int:page>')
-def home():
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, 10)
-    recent, top_tags = sidebar_data()
-    return render_template('home.html', posts=posts, recent=recent, top_tags=top_tags)
-
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    post = Post.query.get_or_404(post_id)
-    tags = post.tags
-    comments = post.comments.order_by(Comment.date.desc()).all()
-    recent, top_tags = sidebar_data()
-
-    return render_template('post.html', post=post, tags=tags, comments=comments, recent=recent, top_tags=top_tags)
-
-@app.route('/tag/<string:tag_name>')
-def tag(tag_name):
-    tag = Tag.query.filter_by(title=tag_name).first_or_404()
-    posts = tag.posts.order_by(Post.publish_date.desc()).all()
-    recent, top_tags = sidebar_data()
-    return render_template('tag.html', tag=tag, posts=posts, recent=recent, top_tags=top_tags)
-
-@app.route('/user/<string:username>')
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.publish_date.desc()).all()
-    recent, top_tags = sidebar_data()
-    return render_template('user.html', user=user, posts=posts, recent=recent, top_tags=top_tags)
-
-
-
-@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
-def post(post_id):
-    form = CommentForm()
-    if form.validate_on_submit():
-        new_comment = Comment()
-    new_comment.name = form.name.data
-    new_comment.text = form.text.data
-    new_comment.post_id = post_id
-    new_comment.date = datetime.datetime().now()
-    db.session.add(new_comment)
-    db.session.commit()
-    post = Post.query.get_or_404(post_id)
-    tags = post.tags
-    comments = post.comments.order_by(Comment.date.desc()).all()
-    recent, top_tags = sidebar_data()
-
-    return render_template('post.html', post=post, tags=tags, comments=comments, recent=recent, top_tags=top_tags, form=form)
-
-
 
 
 if __name__ == '__main__':
--- webapp/webapp/controllers/blog.py
+++ webapp/webapp/controllers/blog.py
@@ -1,10 +1,10 @@
 import datetime
 from os import path
 from sqlalchemy import func
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect
 
 from webapp.models import db, Post, Tag, Comment, User, tags
-from webapp.forms import CommentForm
+from webapp.forms import CommentForm, PostForm
 
 
 blog_blueprint = Blueprint('blog', __name__, template_folder=path.join(path.pardir, 'templates', 'blog'), url_prefix="/blog")
@@ -14,3 +14,37 @@ def sidebar_data():
     recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all
     top_tags = db.session.query(Tag, func.count(tags.c.post_id).label('total')).join(tags).group_by(Tag).order_by('total DESC').limit(5).all()
     return recent, top_tags
+
+
+@blog_blueprint.route('/new', methods=['POST', 'GET'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        new_post = Post(form.title.data)
+        new_post.text = form.text.data
+        new_post.publish_date = datetime.datetime.now()
+
+        db.session.add(new_post)
+        db.session.commit()
+    return render_template('new.html', form=form)
+
+
+@blog_blueprint.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post():
+    post = Post.query.get_or_404(id)
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.text = form.text.data
+        post.publish_date = datetime.datetime.now()
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('.post', post_id=post.id))
+    
+    form.text.data = post.text
+
+    return render_template('edit.html', form=form, post=post)
+    
